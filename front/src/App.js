459c64be5d206608b38623b95ecb6e52f7be28e9
matannagar <73398186+matannagar@users.noreply.github.com>
2022-06-28 15:58:25 +0300

    working on design, green search button now working. there is an error with the server - needs to handle empty string

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
+import image from './assets/logo.jpg'
 
 const api = {
   key: "f0590670e28f43129d763516222806",
   base: "https://api.weatherapi.com/v1/"
 }
 
-
 function App() {
   const [query, setQuery] = useState('')
   const [weather, setWeather] = useState({})
 
   const search = evt => {
-    if (evt.key === "Enter") {
+    console.log(evt)
+    if (evt.key === "Enter" || evt.type === "click") {
       fetch(`http://localhost:3001/getWeatherInfo/city?city=${query}`,
         {
           method: "GET",
@@ -29,7 +30,7 @@ function App() {
     }
   }
 
-  const dateBuilder = (d) => {
+  const dateGenerator = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -44,32 +45,61 @@ function App() {
   return (
     <div className="app">
       <main>
-        <div className="search-box">
-          <input type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
-        {(typeof weather.location != "undefined") ? (
-          <div>
-            <div className="location-box">
-              <div className="location">{weather.location.name}</div>
-              <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="weather-box">
-              <div className="temp">
-                {weather.current.temp_c}°c
-              </div>
-              {/* <div className="weather">{weather.weather[0].main}</div> */}
-            </div>
+        <div className="split left">
+          <img alt="logo" src={image} height={100} width={150} />
+          <div className="intro">
+            Use our weather app to see the weather around the world
           </div>
-        ) : ('')}
-      </main>
 
-    </div>
+          <div className="search-box">
+            <input type="text"
+              className="search-bar"
+              placeholder="Search..."
+              onChange={e => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+            />
+            <button id="check"
+              onChange={e => setQuery(e.target.value)}
+              value={query}
+              onClick={search}>Check</button>
+          </div>
+        </div>
+        <div className="split right">
+          {(typeof weather.location != "undefined") ? (
+            <div className="outer-box">
+              <div className="location-box">
+                <div className="location">{weather.location.name}</div>
+                <div className="country">{weather.location.country}</div>
+                <div className="date">{dateGenerator(new Date())}</div>
+              </div>
+              <div className="weather-box">
+                <div className="temp">
+                  {weather.current.temp_c}°c
+                </div>
+                <div className="weather">{weather.current.condition.text}</div>
+                <div className="row">
+                  <div className="col">
+                    Percipation<br />
+                    {(weather.current.percip_mm !== 0.0) ? (<b>0 mm</b>) : (<b>{weather.current.percip_mm} mm</b>)}
+                  </div>
+                  <div className="col">
+                    Humidity<br />
+                    <b>{weather.current.humidity}%</b>
+                  </div>
+                  <div className="col">
+                    Wind <br />
+                    <b>{weather.current.wind_kph} km/h</b>
+                  </div>
+                </div>
+                {/* + weather.current.percip_mm + } */}
+              </div>
+            </div>
+          ) : ('')}
+        </div>
+      </main >
+
+    </div >
   );
 }
 
