24edacd2cfff3a1751fa78c5886aa91356b3d2c2
matannagar <73398186+matannagar@users.noreply.github.com>
2022-07-17 21:48:52 +0300

    clean code

--- front/src/App.js
+++ front/src/App.js
@@ -10,12 +10,6 @@ function App() {
   const [query, setQuery] = useState('')
   const [weather, setWeather] = useState({})
 
-  /*
-    Search function is activated either by clicking enter or clicking the search button.
-    evt is a type of event that we can check before proceeding with accessing our Node.js server.
-    We are extracting from the server the current weather information and placing it inside 'weather' variable.
-    we then reset the query to be "" again.
-  */
   const search = evt => {
     if (query !== '') {
       if (evt.key === "Enter" || evt.type === "click") {
@@ -35,7 +29,6 @@ function App() {
           })
       }
     }
-
   }
 
   const dateGenerator = (d) => {
@@ -71,24 +64,18 @@ function App() {
 
         {/* RIGHT SIDE */}
         <div className="split right">
-          {/*
-          check if there is a response, if not leave the page empty
-           */}
           {(typeof weather.location != "undefined") ? (
             <div className="outer-box">
               <Location
                 weather={weather}
                 dateGenerator={dateGenerator}
               />
-
               <Weather weather={weather} />
             </div>
           ) : ('')}
-
         </div>
         {/* right-side */}
       </main>
-
     </div >
   );
 }
